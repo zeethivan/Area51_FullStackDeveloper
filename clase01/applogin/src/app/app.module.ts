0b50d09d8f19fdaf7d30998ff54f54607308f4cd
Your Name <you@example.com>
2019-10-29 15:29:10 -0500

    clase01

---
+++ clase01/applogin/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login.component';
+@NgModule({
+  declarations: [
+    AppComponent, LoginComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [LoginComponent]
+})
+export class AppModule { }
---
+++ clase01/applogin/src/app/login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'login',
+  template: '<h1>Home</h1>'
+})
+export class LoginComponent {
+
+}
